a0063d4e397829cae11d9885f8ec36a18f04638c
ch-yx <cuiweixiaoxue63@126.com>
2023-10-13 08:06:36 +0800

    restart

--- src/main/java/name/modid/ExampleMod.java
+++ src/main/java/name/modid/ExampleMod.java
@@ -1,32 +1,15 @@
 package name.modid;
 
 import net.fabricmc.api.ModInitializer;
-import net.minecraft.network.packet.s2c.play.SetCameraEntityS2CPacket;
-import net.minecraft.server.network.ServerPlayerEntity;
-
-import java.util.ArrayList;
-import java.util.Iterator;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import carpet.script.CarpetContext;
+import carpet.script.CarpetEventServer;
 import carpet.script.CarpetExpression;
-import carpet.script.value.Value;
-import carpet.script.LazyValue;
+import carpet.script.argument.FunctionArgument;
 import carpet.script.exception.InternalExpressionException;
-import carpet.script.value.AbstractListValue;
-import carpet.script.value.EntityValue;
-import carpet.script.value.LazyListValue;
-import carpet.script.value.ListValue;
-import carpet.script.value.NumericValue;
-import carpet.script.value.StringValue;
-
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.LongStream;
-
-
+import carpet.script.value.FunctionValue;
+import carpet.script.value.Value;
 import carpet.CarpetExtension;
 import carpet.CarpetServer;
 
@@ -34,167 +17,33 @@ public class ExampleMod implements CarpetExtension, ModInitializer {
 
     public static final Logger LOGGER = LoggerFactory.getLogger("itertool");
     public static final ExampleMod ins = new ExampleMod();
+    public static FunctionValue f;
 
     @Override
     public void onInitialize() {
         CarpetServer.manageExtension(ins);
-        LOGGER.info("itertools loaded");
+        f=null;
+        LOGGER.info(funname+" loaded");
     }
 
+    public void onGameStarted(){
+        f=null;
+    }
+
+    String funname="villager_trade_ovr";
+    
+
     public void scarpetApi(CarpetExpression ce) {
-        var expression = ce.getExpr();
-        // expression.addFunction("test",(lv)->StringValue.of(
-        // lv.get(0).getVariable()+"|||"+lv.get(0).hashCode()+"\n"+
-        // lv.get(1).getVariable()+"|||"+lv.get(1).hashCode()+"\n"+
-        // (lv.get(0)==lv.get(1))
-
-        // ));
-        expression.addContextFunction("spec", 2, (c, t, lv) -> {
-            var player = lv.get(0);
-            var id = lv.get(1);
-            if (id instanceof StringValue sid) {
-                var e = EntityValue.getEntitiesFromSelector(((CarpetContext) c).source(), sid.getString()).stream()
-                        .findFirst();
-                id = EntityValue.of(e.orElse(null));
+        var expression=ce.getExpr();
+        expression.addFunctionWithDelegation(funname, 1, false, false, (c, t, expr, tok, lv) ->
+        {
+            if (lv.isEmpty())
+            {
+                f=null;
             }
-            if (player instanceof EntityValue pv && pv.getEntity() instanceof ServerPlayerEntity pe
-                    && id instanceof EntityValue target) {
-                pe.networkHandler.sendPacket(new SetCameraEntityS2CPacket(target.getEntity()));
-                return Value.TRUE;
-            }
-            return Value.NULL;
+            FunctionArgument functionArgument = FunctionArgument.findIn(c, expression.module, lv, 0, false, true);
+            f = functionArgument.function;
+            return Value.TRUE;
         });
-        expression.addLazyFunction("generator", -1, (c, t, lv) -> {
-
-            return (cc, tt) -> new LazyListValue() {
-                LazyValue State = (c, t) -> Value.NULL.reboundedTo("_");
-                LazyValue nextexpr = lv.get(0);
-                LazyValue hasNextexpr = lv.get(1);
-
-                @Override
-                public boolean hasNext() {
-                    LazyValue _val = c.getVariable("_");
-                    c.setVariable("_", State);
-                    try {
-                        Value result = hasNextexpr.evalValue(c, carpet.script.Context.Type.BOOLEAN);
-                        return result.getBoolean();
-                    } finally {
-                        c.setVariable("_", _val);
-                    }
-                }
-
-                @Override
-                public Value next() {
-                    LazyValue _val = c.getVariable("_");
-                    c.setVariable("_", State);
-                    try {
-                        Value result = nextexpr.evalValue(c, t);
-                        return result;
-                    } finally {
-                        State = c.getVariable("_");
-                        c.setVariable("_", _val);
-                    }
-                }
-
-                @Override
-                public void reset() {
-                    if (lv.size() > 2) {
-                        LazyValue _val = c.getVariable("_");
-                        c.setVariable("_", State);
-                        try {
-                            lv.get(2).evalValue(c, t);
-                        } finally {
-                            c.setVariable("_", _val);
-                        }
-
-                    }
-                    State = (c, t) -> Value.NULL.reboundedTo("_");
-                }
-
-            };
-        });
-
-        // script run
-        // cart_product(...iters)->(res=[[]];for(iters,iter=_;newres=[];for(res,ent=_;for(iter,put(newres,null,[...ent,_]);));res=newres;);return
-        // (res););
-        // cart_product([1,2,3,4],[5,6,7],[8])
-        expression.addFunction("cartesian_product", list_of_iterable_values -> {
-
-            ArrayList<ArrayList<Value>> res = new ArrayList<ArrayList<Value>>();
-            res.add(new ArrayList<Value>());
-
-            for (Value a_iterable : list_of_iterable_values) {
-                ArrayList<ArrayList<Value>> nres = new ArrayList<ArrayList<Value>>();
-                for (ArrayList<Value> eres : res) {
-                    ArrayList<Value> _eres = null;
-                    for (Value a : ((AbstractListValue) a_iterable).unpack()) {
-                        _eres = new ArrayList<>();
-                        _eres.addAll(eres);
-                        _eres.add(a);
-                        nres.add(_eres);
-                    }
-                }
-                res = nres;
-            }
-            return ListValue.wrap(res.stream().map(ListValue::wrap));
-        });
-        expression.addFunction("zip", (list_of_iterable_values) -> {
-            /*
-             * if(list_of_iterable_values.size()<1){
-             * throw new
-             * InternalExpressionException("There is no arguments of 'zip' function");
-             * }
-             */
-            if (list_of_iterable_values.stream().allMatch(
-                    v -> (v instanceof AbstractListValue || v instanceof NumericValue || v instanceof StringValue))) {
-                return new LazyListValue() {
-                    Iterator<? extends Value> fromValuetoIterator(Value v) {
-                        if (v instanceof AbstractListValue) {
-                            return ((AbstractListValue) v).iterator();
-                        } else if (v instanceof NumericValue) {
-                            return LongStream.range(0, NumericValue.asNumber(v).getLong()).mapToObj(NumericValue::new)
-                                    .iterator();
-                        } else if (v instanceof StringValue sv) {
-                            return sv.getString().codePoints().mapToObj(Character::toChars).map(String::new)
-                                    .map(StringValue::new).iterator();
-                        } else
-                            return null;
-                    }
-
-                    // List<AbstractListValue>
-                    // iterator_value_list=list_of_iterable_values.stream().map(v->(AbstractListValue)v).collect(Collectors.toList());
-                    List<Iterator<? extends Value>> iterator_list = list_of_iterable_values.stream()
-                            .map(v -> fromValuetoIterator(v)).collect(Collectors.toList());
-
-                    @Override
-                    public boolean hasNext() {
-                        return iterator_list.stream().anyMatch(Iterator::hasNext);
-                    }
-
-                    @Override
-                    public Value next() {
-                        return ListValue.wrap(iterator_list.stream().map((v) -> v.hasNext() ? v.next() : Value.NULL));
-                    }
-
-                    @Override
-                    public void reset() {
-                        list_of_iterable_values.stream().filter(v -> v instanceof LazyListValue)
-                                .forEach(v -> ((LazyListValue) v).reset());
-                        // this.iterator_value_list=list_of_iterable_values.stream().map(v->(AbstractListValue)v).collect(Collectors.toList());
-                        this.iterator_list = list_of_iterable_values.stream().map(v -> fromValuetoIterator(v))
-                                .collect(Collectors.toList());
-                    }
-
-                    @Override
-                    public String getString() {
-                        return "[zip_of_" + list_of_iterable_values.size() + "...]";
-                    }
-                };
-            } else {
-                throw new InternalExpressionException("All argument of 'zip' function should be a list or iterator");
-            }
-
-        });
-
     }
 }
